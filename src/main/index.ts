02bdf0eaa6ad568e5fa8babd6743c071aae64070
zouyaoji <370681295@qq.com>
2021-07-07 10:29:14 +0800

    update

--- src/main/index.ts
+++ src/main/index.ts
@@ -3,14 +3,15 @@
  */
 import '@src/common/patch'
 import { join } from 'path'
-import { app, BrowserWindow, globalShortcut } from 'electron'
+import { app, BrowserWindow, globalShortcut, MenuItem, MenuItemConstructorOptions, Menu } from 'electron'
 import dotenv from 'dotenv'
+const isMac = process.platform === 'darwin'
 
 dotenv.config({ path: join(__dirname, '../../.env') })
 
 let win: BrowserWindow
 
-function createWin() {
+function createWin () {
   // 创建浏览器窗口
   win = new BrowserWindow({
     width: 1440,
@@ -28,11 +29,82 @@ function createWin() {
 
   // win.webContents.openDevTools()
 
+
+
   const URL = app.isPackaged
     ? `file://${join(__dirname, '../render/index.html')}` // vite 构建后的静态文件地址
     : `http://localhost:${process.env.PORT}` // vite 启动的服务器地址
 
   win?.loadURL(URL)
 }
+
+const template: Array<(MenuItemConstructorOptions) | (MenuItem)> = [
+  {
+    label: 'File',
+    submenu: [
+      isMac ? { role: 'close' } : { role: 'quit' }
+    ]
+  },
+  // { role: 'editMenu' }
+  {
+    label: 'Edit',
+    submenu: [
+      { role: 'undo' },
+      { role: 'redo' },
+      { type: 'separator' },
+      { role: 'cut' },
+      { role: 'copy' },
+      { role: 'paste' },
+      { role: 'delete' },
+      { type: 'separator' },
+      { role: 'selectAll' }
+    ]
+  },
+  // { role: 'viewMenu' }
+  {
+    label: 'View',
+    submenu: [
+      { role: 'reload' },
+      { role: 'forceReload' },
+      { role: 'toggleDevTools' },
+      { type: 'separator' },
+      { role: 'resetZoom' },
+      { role: 'zoomIn' },
+      { role: 'zoomOut' },
+      { type: 'separator' }
+    ]
+  },
+  // { role: 'windowMenu' }
+  {
+    label: 'Window',
+    submenu: [
+      { role: 'minimize' },
+      { role: 'zoom' },
+      { role: 'close' }
+    ]
+  },
+  {
+    role: 'help',
+    submenu: [
+      {
+        label: 'Learn More',
+        click: async () => {
+          const { shell } = require('electron')
+          await shell.openExternal('https://electronjs.org')
+        }
+      },
+      {
+        label: 'VueCesium Document',
+        click: async () => {
+          const { shell } = require('electron')
+          await shell.openExternal('https://zouyaoji.top/vue-cesium')
+        }
+      }
+    ]
+  }
+]
+
+const menu = Menu.buildFromTemplate(template)
+Menu.setApplicationMenu(menu)
 
 app.whenReady().then(createWin)
